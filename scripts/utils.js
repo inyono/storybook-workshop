8112d7460aed46289435b9f479d4b3b276edaafd
Jonas Keinholz <jonas@keinholz.com>
2017-10-27 15:13:02 +0200

    fix: ci

--- scripts/utils.js
+++ scripts/utils.js
@@ -2,7 +2,7 @@ const spawn = require("cross-spawn");
 const path = require("path");
 
 module.exports.runNpmTask = (package, task, args = []) => {
-  spawn.sync("npm", ["run", task, "--", ...args], {
+  spawn.sync("npm", [...(task === "install" ? []: ["run"]), task, "--", ...args], {
     cwd: path.join(__dirname, "..", package),
     stdio: "inherit"
   });
