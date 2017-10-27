ed70d6f9edcfea191d79ace77941b38834740f8f
Jonas Keinholz <jonas@keinholz.com>
2017-10-27 10:50:39 +0200

    feat(mobile): add router

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,27 +1,30 @@
+import { Container } from "native-base";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NativeRouter, Route, Link } from "react-router-native";
 
+import Login from "./login";
+import Dashboard from "./Dashboard";
 import { loadFonts } from "./utils";
 
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+const App = () => (
+  <NativeRouter>
+    <Container>
+      <Route
+        exact
+        path="/"
+        render={({ history }) => (
+          <Login
+            onSubmit={username => history.push(`/dashboard/${username}`)}
+          />
+        )}
+      />
+      <Route
+        path="/dashboard/:username"
+        render={({ match }) => <Dashboard username={match.params.username} />}
+      />
+    </Container>
+  </NativeRouter>
+);
 
 export default loadFonts(App);
